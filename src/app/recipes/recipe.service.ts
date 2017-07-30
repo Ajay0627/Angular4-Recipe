9d355f14b4515447f8130e21f165e11cbacec160
Ajay <ajaybaluguri0627@gmail.com>
2017-07-30 12:14:02 -0400

    Added Services

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,37 @@
+import { EventEmitter, Injectable} from '@angular/core';
+
+import { Recipe} from './recipe.model';
+import { Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
+
+
+@Injectable()
+export class RecipeService {
+	recipeSelected = new EventEmitter<Recipe>();
+	private recipes: Recipe[] = [
+		new Recipe('A Test Recipe', 'This is simply a test', 
+			'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg',
+			 [
+			 	new Ingredient('Meat', 1),
+			 	new Ingredient('leamon',2)
+			 ]),
+		new Recipe('Recipe Test', 'This is a test', 
+			'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg', 
+			 [
+			 new Ingredient('Meat', 1),
+			 	new Ingredient('leamon',2)
+
+			 ])
+
+    ];
+    constructor(private slService: ShoppingListService) {}
+
+    getRecipes() {
+    	return this.recipes.slice();
+    }
+
+    AddIngredientsToShoppingList(ingredients: Ingredient[]) {
+    	this.slService.addIngredients(ingredients);
+    }
+
+}
