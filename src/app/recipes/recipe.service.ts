36eace2c4554bdc13e0baeaa0d2a30c54f1968fe
Ajay <ajaybaluguri0627@gmail.com>
2017-08-24 18:47:57 -0400

    authuntication

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -17,13 +17,14 @@ export class RecipeService {
 			 	new Ingredient('Meat', 1),
 			 	new Ingredient('leamon',2)
 			 ]),
-		new Recipe('Recipe Test', 'This is a test', 
-			'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg', 
-			 [
-			 new Ingredient('Meat', 1),
-			 	new Ingredient('leamon',2)
+		
+		new Recipe('Speghetti', 'This is a recipe', 
+ 			 'https://upload.wikimedia.org/wikipedia/commons/7/70/Spaghetti_alle_vongole.jpg',			 [
+			 new Ingredient('Speghetti', 2),
+			 	new Ingredient('Cilantro',1)
 
 			 ])
+		
 
     ];
     constructor(private slService: ShoppingListService) {}
